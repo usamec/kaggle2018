c51024946132414384e073b439fc6552a9b4c40a
Vlado Boza <vlado.boza@ceai.io>
2018-11-26 21:34:32 +0100

    best 1515762.5100146034

--- updater/src/bin/kopt.rs
+++ updater/src/bin/kopt.rs
@@ -80,11 +80,44 @@ fn do_opt(tour: &mut Tour, candidates: &[Vec<(usize, f64)>], temp: f64) -> Optio
     }
 }
 
+fn local_update<F>(size: usize, start: usize, temp: f64, nodes: &[(f64, f64)], current_tour: &[usize], cur_len: f64, primes: &[bool],
+                   inner: F) -> Option<(Vec<usize>, f64)>
+    where F: Fn(&mut [usize], &[(f64, f64)], &[bool], usize) -> bool {
+    let end = start + size;
+
+    let mut slice_and_padding = current_tour[start - 1..end + 1].to_owned();
+    let old_len = calculate_len(&nodes, &slice_and_padding, &primes, start - 1);
+    let mut rng = rand::thread_rng();
+    if inner(&mut slice_and_padding, &nodes, &primes, start - 1) {
+        let new_len = calculate_len(&nodes, &slice_and_padding, &primes, start - 1);
+        if new_len < old_len || (temp > 0.0 && ((old_len - new_len) / temp).exp() > rng.gen::<f64>()) {
+            println!("boom {} {} {} {}", old_len, new_len, old_len - new_len, size);
+            let mut new_tour = current_tour.to_vec();
+            {
+                let slice = &mut new_tour[start - 1..end + 1];
+                for i in 0..slice_and_padding.len() {
+                    slice[i] = slice_and_padding[i];
+                }
+            }
+            let new_len = verify_and_calculate_len(&nodes, &new_tour, &primes);
+            println!("new_len {}", new_len);
+            if new_len < cur_len {
+                println!("better {:?}", new_len);
+            }
+            Some((new_tour, new_len))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
 fn main() {
-    let n_threads = 20;
+    let n_threads = 10;
     let nodes = Arc::new(load_poses());
     let primes = Arc::new(get_primes(nodes.len()));
-    let tour = Arc::new(Mutex::new(Tour::new(load_tour("../outputs/kopt7-0.csv"), nodes.clone(), primes.clone())));
+    let tour = Arc::new(Mutex::new(Tour::new(load_tour("../outputs/kopt6-0.csv"), nodes.clone(), primes.clone())));
     let candidates = load_candidates();
     let candidates_w = candidates.iter().enumerate().map(|(i, cc)| {
         cc.iter().map(|&c| (c, 1.0 / dist(nodes[i], nodes[c]).ln_1p())).collect::<Vec<_>>()
@@ -107,7 +140,7 @@ fn main() {
                     our_tour = new_tour;
                     let mut main_tour = main_tour_mutex.lock().unwrap();
                     *main_tour = our_tour.clone();
-                    our_tour.save(&format!("../outputs/kopt7-{}.csv", thread_id));
+                    our_tour.save(&format!("../outputs/kopt6-{}.csv", thread_id));
                 }
                 cc += 1;
                 if cc % 1000000 == 0 {
@@ -115,6 +148,36 @@ fn main() {
                     our_tour = main_tour.clone();
                     println!("cc {}", cc);
                 }
+            }
+        });
+        handles.push(handle);
+    }
+
+    for thread_id in 0..n_threads {
+        let main_tour_mutex = Arc::clone(&tour);
+        let our_nodes = nodes.clone();
+        let our_primes = primes.clone();
+        let handle = thread::spawn(move || {
+            let mut cc = 0;
+            let mut our_tour = main_tour_mutex.lock().unwrap().get_path().to_vec();
+            let mut cur_len = verify_and_calculate_len(&our_nodes, &our_tour, &our_primes);
+            let mut rng = rand::thread_rng();
+            loop {
+                let size = rng.gen_range(40, 41);
+                let start = rng.gen_range(1, our_tour.len() - size - 1);
+                let maybe_new_tour = local_update(size, start, 0.0, &our_nodes, &our_tour, cur_len, &our_primes, full_optim);
+                if let Some((new_tour, new_len)) = maybe_new_tour {
+                    println!("better brute {} {}", cur_len, new_len);
+                    cur_len = new_len;
+                    our_tour = new_tour;
+                    let mut main_tour = main_tour_mutex.lock().unwrap();
+                    *main_tour = Tour::new(our_tour.clone(), our_nodes.clone(), our_primes.clone());
+                }
+                cc += 1;
+                let main_tour = main_tour_mutex.lock().unwrap();
+                our_tour = main_tour.get_path().to_vec();
+                cur_len = verify_and_calculate_len(&our_nodes, &our_tour, &our_primes);
+                println!("ccb {}", cc);
             }
         });
         handles.push(handle);
